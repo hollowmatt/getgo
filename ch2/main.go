f477830c10a92f09b7f1bfbd974cd89296a7c2fa
Matt Holloway <hollowmatt@gmail.com>
2025-11-01 16:20:21 +0000

    v2

--- ch2/main.go
+++ ch2/main.go
@@ -1,16 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	text := "Let's count some words"
-	var numSpaces int
-
-	for i := 0; i < len(text); i++ {
-		if text[i] == ' ' {
-			numSpaces++
-		}
-	}
-
-	fmt.Println("found", numSpaces+1, "words")
+	words := strings.Fields(text)
+	fmt.Println("found", len(words), "words")
+	fmt.Println(words)
 }
