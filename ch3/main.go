be8c106a62520849299bbb2a7d8f520758c75f3b
Matt Holloway <hollowmatt@gmail.com>
2025-11-11 19:57:29 +0000

    Structs

--- ch3/main.go
+++ ch3/main.go
@@ -2,24 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strconv"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		log.Println("Please provide a name and age as arguments")
-		return
-	}
-
-	var person struct {
+	// 3.6 Structs
+	type person struct {
 		name string
 		age  int64
 	}
-	person.name = os.Args[1]
-	person.age, _ = strconv.ParseInt(os.Args[2], 10, 64)
 
-	fmt.Printf("Hello, my name is %s and I am %d years old.", person.name, person.age)
+	// How to get an int from the args
+	//person.age, _ = strconv.ParseInt(os.Args[2], 10, 64)
+	people := []person{
+		{
+			name: "Dave",
+			age:  30,
+		},
+		{
+			name: "Jane",
+			age:  25,
+		},
+	}
+	for _, person := range people {
+		fmt.Printf("Hello, my name is %s and I am %d years old\n", person.name, person.age)
+	}
+
+	// 3.7 Pointers
 
 }
