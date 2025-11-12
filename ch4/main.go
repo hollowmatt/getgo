36b6a8dfa9c1fa268f53ec4fa6c5e197c0dd75f9
Matt Holloway <hollowmatt@gmail.com>
2025-11-12 13:14:16 +0000

    setup Ch4

---
+++ ch4/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// starting point for chapter 4
+func main() {
+	fmt.Println("Chapter 4")
+}
+
+// 4.1 Arrays
+func arrays() {
+}
+
+// 4.2 Slices
+func slices() {
+}
+
+// 4.3 Maps
+func maps()
