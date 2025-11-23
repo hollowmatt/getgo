2788730ec51daac4f0be52f5e03b0bb5739e5099
Matt Holloway <hollowmatt@gmail.com>
2025-11-23 14:17:31 +0000

    end

--- ch7/main.go
+++ ch7/main.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("Ch 7")
+	var store Store
+	err := store.Put("key1", "value1")
+	if err != nil {
+		fmt.Println("Error putting value:", err)
+		return
+	}
+}
+
+type Store interface {
+	Put(key string, val string) error
+	Get(key string) (string, error)
+	Delete(key string) error
 }
